183b58f9cf94b26cc15495e323d7c70eb2e71589
RobertDivkovic <robertdivkovic98@gmail.com>
2025-04-10 04:56:39 +0200

    Added automatic extraction of healthy and powdery mildew zip datasets

--- src/data_management.py
+++ src/data_management.py
@@ -1,18 +1,23 @@
 import os
 import joblib
 import gdown
+import zipfile
 from tensorflow.keras.models import load_model
 
 # Google Drive File IDs
 MODEL_ID = "1GYpG0YDaNUGaxTx5c4CgVp5rbksmiptP"
 IMAGE_SHAPE_ID = "1eAlTBXjbEh6BslNDvTV3CqrECcFeAW8j"
 CLASS_INDICES_ID = "1yLJPT2gQpijFCXzE9ogl6Desrmteka2e"
+POWDERY_MILDEW_ZIP_ID = "1lVl9K68iQ4X6METcyZtH8xqEq42zxcee"
+HEALTHY_ZIP_ID = "1o1Q-iih2WCFrPYohXWevDAvm4zdeI7pH"
 
 # Local paths
 MODEL_PATH = "outputs/v1/cherry_leaf_mildew_model.h5"
 IMAGE_SHAPE_PATH = "outputs/02_data_visualisation/image_shape.pkl"
 CLASS_INDEX_PATH = "outputs/03_modelling_and_evaluating/class_indices.pkl"
-
+POWDERY_ZIP_PATH = "inputs/powdery_mildew.zip"
+HEALTHY_ZIP_PATH = "inputs/healthy.zip"
+EXTRACTED_DATASET_PATH = "inputs"
 
 def is_h5_valid(path):
     try:
@@ -25,7 +30,6 @@ def is_h5_valid(path):
 
 
 def download_if_missing(path, file_id):
-    # Always delete and re-download on Heroku
     if "DYNO" in os.environ and os.path.exists(path):
         print(f"[Heroku] Deleting existing file to force re-download: {path}")
         os.remove(path)
@@ -39,15 +43,27 @@ def download_if_missing(path, file_id):
         print(f"[Download] File size: {os.path.getsize(path)} bytes")
 
 
+def unzip_if_needed(zip_path, extract_to):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+        print(f"[Unzip] Extracted {zip_path} to {extract_to}")
+
+
 def load_model_and_metadata(
     model_path=MODEL_PATH,
     image_shape_path=IMAGE_SHAPE_PATH,
     class_index_path=CLASS_INDEX_PATH
 ):
-    # Download files
+    # Download model and metadata
     download_if_missing(model_path, MODEL_ID)
     download_if_missing(image_shape_path, IMAGE_SHAPE_ID)
     download_if_missing(class_index_path, CLASS_INDICES_ID)
+    download_if_missing(POWDERY_ZIP_PATH, POWDERY_MILDEW_ZIP_ID)
+    download_if_missing(HEALTHY_ZIP_PATH, HEALTHY_ZIP_ID)
+
+    # Unzip datasets
+    unzip_if_needed(POWDERY_ZIP_PATH, EXTRACTED_DATASET_PATH)
+    unzip_if_needed(HEALTHY_ZIP_PATH, EXTRACTED_DATASET_PATH)
 
     # Check model integrity before loading
     if not is_h5_valid(model_path):
@@ -56,7 +72,7 @@ def load_model_and_metadata(
     print(f"[Model] Loading model from: {model_path}")
     print(f"[Model] Model file size: {os.path.getsize(model_path)} bytes")
 
-    # Load content
+    # Load the files
     model = load_model(model_path)
     image_shape = joblib.load(image_shape_path)
     class_indices = joblib.load(class_index_path)
